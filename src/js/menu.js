c20e7653db69dd9a635ef77a881009400dce0ffa
NathanBatistaBalanche <nathan.batista_balanche@edu.univ-fcomte.fr>
2025-01-08 16:05:13 +0100

    Animation menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -4,12 +4,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const closeButton = document.querySelector('.menu--cross__btn');
 
     menuButton.addEventListener('click', function () {
-        menu.style.display = 'block';
-        menu.setAttribute('aria-hidden', 'false');
-    });
+        menu.style.transform = 'translateX(100vw)';
+        menu.style.display = 'block'; 
+        setTimeout(function() {
+            menu.style.transform = 'translateX(0)'; 
+            menu.setAttribute('aria-hidden', 'false');
+        }, 10);
+});
 
     closeButton.addEventListener('click', function () {
-        menu.style.display = 'none';
-        menu.setAttribute('aria-hidden', 'true');
+        setTimeout(function() {
+            menu.style.display = 'none';
+            menu.setAttribute('aria-hidden', 'true');
+        }, 300); 
+        menu.style.transform = 'translateX(100vw)';
     });
 });
